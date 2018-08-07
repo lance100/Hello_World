f3f478e04d2042cf495eac8cdff3d505c569456d
Lance <lance100@comcast.net>
2018-08-07 18:04:37 -0500

    This class was originally supposed to be added, but was instead deleted for some reason?

---
+++ Hello Git/src/ThisClass.java
@@ -0,0 +1,14 @@
+public class ThisClass 
+{
+	private String argument;
+	
+	public ThisClass (String argument)
+	{
+		this.argument = argument;
+	}
+	
+	public void showArgument ()
+	{
+		System.out.println ("The argument shows as: " + argument);
+	}
+}
